277831900592767bd75f1cfc7e1ab820e7595992
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-04-11 10:51:10 +0300

    add text input to add place

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,17 +1,46 @@
 
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React,{useState} from 'react';
+import { ScrollView, View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import Colors from '../constants/Colors';
 
 const NewPlaceScreen = props => {
-    return(
-        <View>
-            <Text>New Places screen</Text>
-        </View>
+    const [titleValue, setTitleValue] = useState('');
+
+const titleChangeHandler = text =>{
+    setTitleValue(text)
+}
+const savePlaceHandler = () => {
+    
+}
+
+    return (
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput} value={titleValue} onChangeText={titleChangeHandler}/> 
+                <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler}/>
+            </View>
+        </ScrollView>
     )
 }
-NewPlaceScreen.navigationOptions ={
-    headerTitle:"Add Place"
+NewPlaceScreen.navigationOptions = {
+    headerTitle: "Add Place"
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    form:{ 
+        margin:30,
+    },
+    label:{
+        fontSize:18,
+        marginBottom:15
+    },
+    textInput:{
+        borderBottomColor:'#ccc',
+        borderBottomWidth:1,
+        marginBottom:15,
+        paddingVertical:4,
+        paddingHorizontal:2
+    },
+})
 
 export default NewPlaceScreen;
